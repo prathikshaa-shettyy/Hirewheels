679ec8b12b1da22b4f7fccfc19c47bd00df4700f
Prathiksha shetty <prathikshashetty47@gmail.com>
2020-11-10 21:59:07 +0530

    First Commit

---
+++ src/main/java/com/upgrad/Hirewheels/HireWheelsApplication.java
@@ -0,0 +1,13 @@
+package com.upgrad.Hirewheels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HireWheelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HireWheelsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upgrad/Hirewheels/HireWheelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upgrad.Hirewheels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HireWheelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
