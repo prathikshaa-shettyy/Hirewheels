10395fcec3e82a8dcc7c2ac9553fa8b382f78046
Prathiksha shetty <prathikshashetty47@gmail.com>
2020-11-11 19:31:10 +0530

    Implementing Restful API

---
+++ src/main/java/com/upgrad/Hirewheels/controllers/VehicleController.java
@@ -0,0 +1,45 @@
+package com.upgrad.Hirewheels.controllers;
+
+import com.upgrad.Hirewheels.dao.VehicleCategoryDAO;
+import com.upgrad.Hirewheels.exceptions.VehicleNotFoundException;
+import org.modelmapper.ModelMapper;
+import com.upgrad.Hirewheels.dto.VehicleDTO;
+import com.upgrad.Hirewheels.entities.Vehicle;
+import com.upgrad.Hirewheels.services.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class VehicleController {
+    @Autowired
+    VehicleService vehicleService;
+    @Autowired
+    VehicleCategoryDAO vehicleCategoryDao;
+    @Autowired
+    ModelMapper modelMapper;
+    @GetMapping(value= {"/hirewheels/v1/vehicles"}, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity getVehicles(@RequestParam(name = "categoryName") String categoryName, @RequestParam(name="pickUpDate") LocalDateTime pickUpDate, @RequestParam(name="dropDate")LocalDateTime dropDate, @RequestParam(name="locationId") int locationId) throws VehicleNotFoundException
+    {
+        List<Vehicle> vehicleList=vehicleService.getAllVehicles();
+        List<VehicleDTO> vehicleDTOList=new ArrayList<>();
+        for(Vehicle vehicle:vehicleList){
+            if(vehicle.getVehicleSubcategory().equals(categoryName)&& vehicle.getLocation().equals(locationId) && vehicle.getBooking().equals(pickUpDate) && vehicle.getBooking().equals(dropDate)) {
+                vehicleDTOList.add(modelMapper.map(vehicle, VehicleDTO.class));
+            }
+            else
+                {
+                    return null;
+                }
+            }
+        return new ResponseEntity<>(vehicleDTOList,HttpStatus.OK);
+    }
+}
+
--- src/main/java/com/upgrad/Hirewheels/dto/VehicleDTO.java
+++ src/main/java/com/upgrad/Hirewheels/dto/VehicleDTO.java
@@ -1,137 +1,74 @@
-package com.upgrad.Hirewheels.entities;
+package com.upgrad.Hirewheels.dto;
 
-import javax.persistence.*;
+import com.upgrad.Hirewheels.entities.FuelType;
+import com.upgrad.Hirewheels.entities.Location;
+import com.upgrad.Hirewheels.entities.VehicleSubCategory;
 import java.util.Set;
-
-public class Vehicle {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+public class VehicleDTO {
     private int vehicleId;
-
-    @Column(length = 50, nullable = false)
     private String vehicleModel;
-
-    @Column(length = 10, nullable = false)
     private String vehicleNumber;
-
-    @Column(length = 50, nullable = false)
     private String color;
-
-    @Column(length = 1, nullable = false)
     private int availableStatus;
-
-    @Column(length = 500, nullable = false)
     private String vehicleImageUrl;
-
-    @OneToMany(mappedBy = "vehicle")
-    private Set<Booking> booking;
-
-    @ManyToOne
-    @JoinColumn(name = "fuelType_id", nullable = false)
     private FuelType fuelType;
-
-
-    @ManyToOne
-    @JoinColumn(name= "location_id", nullable = false)
     private Location location;
-
-    @ManyToOne
-    @JoinColumn(name = "vehicleSubcategory_id", nullable = false)
     private VehicleSubCategory vehicleSubcategory;
-
-    public Vehicle() {
-    }
-
-    public Vehicle(String vehicleModel, String vehicleNumber, VehicleSubCategory vehicleSubcategory, String color, Location location, FuelType fuelType, int availabilityStatus, String vehicleImageUrl) {
-        this.vehicleModel = vehicleModel;
-        this.vehicleNumber = vehicleNumber;
-        this.vehicleSubcategory = vehicleSubcategory;
-        this.color = color;
-        this.location = location;
-        this.fuelType = fuelType;
-        this.availableStatus = availabilityStatus;
-        this.vehicleImageUrl = vehicleImageUrl;
-    }
-
     public int getVehicleId() {
         return vehicleId;
     }
-
     public void setVehicleId(int vehicleId) {
         this.vehicleId = vehicleId;
     }
-
     public String getVehicleModel() {
         return vehicleModel;
     }
-
     public void setVehicleModel(String vehicleModel) {
         this.vehicleModel = vehicleModel;
     }
-
     public String getVehicleNumber() {
         return vehicleNumber;
     }
-
     public void setVehicleNumber(String vehicleNumber) {
         this.vehicleNumber = vehicleNumber;
     }
-
     public String getColor() {
         return color;
     }
-
     public void setColor(String color) {
         this.color = color;
     }
-
     public int getAvailableStatus() {
         return availableStatus;
     }
-
     public void setAvailableStatus(int availableStatus) {
         this.availableStatus = availableStatus;
     }
-
     public String getVehicleImageUrl() {
         return vehicleImageUrl;
     }
-
     public void setVehicleImageUrl(String vehicleImageUrl) {
         this.vehicleImageUrl = vehicleImageUrl;
     }
-
     public FuelType getFuelType() {
         return fuelType;
     }
-
     public void setFuelType(FuelType fuelType) {
         this.fuelType = fuelType;
     }
-
     public Location getLocation() {
         return location;
     }
-
     public void setLocation(Location location) {
         this.location = location;
     }
-
     public VehicleSubCategory getVehicleSubcategory() {
         return vehicleSubcategory;
     }
-
-    public Set<Booking> getBooking() {
-        return booking;
-    }
-
     public void setVehicleSubcategory(VehicleSubCategory vehicleSubcategory) {
         this.vehicleSubcategory = vehicleSubcategory;
     }
+}
 
-    @Override
-    public String toString() {
-        return "Vehicle{" + "vehicleId=" + vehicleId + ", vehicleModel='" + vehicleModel + '\'' + ", vehicleNumber=" + vehicleNumber + ", color='" + color + '\'' + ", availableStatus=" + availableStatus + ", vehicleImageUrl='" + vehicleImageUrl + '\'' + ", fuelType=" + fuelType + ", location=" + location + ", vehicleSubcategory=" + vehicleSubcategory + '}';
-    }
-    }
+
 
